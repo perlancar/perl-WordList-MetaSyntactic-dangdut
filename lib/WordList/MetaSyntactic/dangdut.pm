dc4c74c00ff6582909f7473b07662ed557c2b8d8
perlancar (@pc-home) <perlancar@gmail.com>
2016-06-06 07:13:30 +0700

    Initial commit

---
+++ lib/WordList/MetaSyntactic/dangdut.pm
@@ -0,0 +1,15 @@
+package WordList::MetaSyntactic::dangdut;
+
+# DATE
+# VERSION
+
+use parent qw(WordList::MetaSyntactic);
+
+1;
+# ABSTRACT: Word lists from Acme::MetaSyntactic::dangdut
+
+=head1 SEE ALSO
+
+L<WordList::MetaSyntactic>
+
+L<WordList>
